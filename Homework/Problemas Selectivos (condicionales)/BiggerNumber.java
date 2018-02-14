8f436eb95174128df07411339170638ba70937c1
Angelmarcat <35696114+Angelmarcat@users.noreply.github.com>
2018-02-13 18:44:40 -0600

    remake

---
+++ Homework/Problemas Selectivos (condicionales)/BiggerNumber.java
@@ -0,0 +1,24 @@
+/*********************************************************************************************************
+**************      Angel Guillermo  Martinez Cruz                                 ***********************
+**************      BiggerNumber.java                                                ***********************
+**************      This program verify the bigger number                          ***********************
+**********************************************************************************************************/
+import javax.swing.JOptionPane;
+public class BiggerNumber
+{
+  public static void main(String[] args)
+  {
+    int a,b,m;
+    a=Integer.parseInt(JOptionPane.showInputDialog("Enter a number: "));
+    b=Integer.parseInt(JOptionPane.showInputDialog("Enter other number: "));
+    if(a>b)
+    {
+      m=a;
+    }
+    else
+    {
+      m=b;
+    }
+    JOptionPane.showMessageDialog(null,"The bigger number is: "+m);
+  }
+}
